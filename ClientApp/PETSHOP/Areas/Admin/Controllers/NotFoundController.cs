1b416b9159b3b578921711560b75f830e72e7470
MSI\ADMIN <sanglun2254@gmail.com>
2020-07-01 00:19:48 +0700

    Update Notfound page

---
+++ ClientApp/PETSHOP/Areas/Admin/Controllers/NotFoundController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PETSHOP.Common;
+
+namespace PETSHOP.Areas.Admin.Controllers
+{
+    [Area(Constants.ADMIN)]
+    public class NotFoundController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
